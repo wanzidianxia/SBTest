646f088ee8a40f614dcea33aa82e877b6d6e8dfd
wanzidianxia <553628742@qq.com>
2019-02-13 16:27:32 +0800

    添加geterseter

--- src/main/java/com/springboot/test/demo/chapters3/pojo/User.java
+++ src/main/java/com/springboot/test/demo/chapters3/pojo/User.java
@@ -1,6 +1,30 @@
 package com.springboot.test.demo.chapters3.pojo;
 
 public class User {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
     private  Long id;
 
     private  String userName;
